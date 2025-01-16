05d139b3c9a7ba5d2eedfe0d5f3d5a2c762ea0a5
AI-NET-by <korotych.vladiclav@gmail.com>
2025-01-16 03:13:02 +0300

    version 2.1

--- scripts/run_aut_lk.sh
+++ scripts/run_aut_lk.sh
@@ -4,6 +4,6 @@ DIR=$(dirname "$0")
 
 FILE="$DIR/../tests/locustfile.py"
 
-start http://localhost:8089/
+start http://localhost:8089
 
 locust -f "$FILE" --host https://ru.pinterest.com --run-time 10 --autostart --autoquit 10
